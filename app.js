44f48fe56b0eaea0f8539d6547c8230d35ce38aa
Andres Brito <74308844+AndresPartnersly@users.noreply.github.com>
2024-12-22 17:18:32 -0400

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const sql = postgres({
     port                 : process.env.DB_PORT,          // Postgres server port[s]
     database             : process.env.DB_NAME,            // Name of database to connect to
     username             : process.env.DB_USER,            // Username of database user
-    password             : process.env.DB_PASSWORD,            // Password of database user
+    password             : process.env.DB_PASSWORD,
     idle_timeout: 60 * 1000,
     connect_timeout: 1200
   });
@@ -25,32 +25,32 @@ app.use(express.json());
 // Endpoint para realizar una consulta básica con parámetros dinámicos
 app.get('/search', async (req, res) => {
 
-    const cuit = req[`query`][`cuit`].toString();
-    const dia = parseInt(req[`query`][`dia`]);
-    const mes = parseInt(req[`query`][`mes`]);
-    const ano = parseInt(req[`query`][`ano`]);
-
-    console.log(`CUIT: ${cuit} - DIA: ${dia} - MES: ${mes} - AñO: ${ano} \n`);
-  
+    const cuit = req[`query`][`cuit`];
+    const periodo_desde = req[`query`][`desde`];
+    const periodo_hasta = req[`query`][`hasta`];
+    const tipo = parseInt(req[`query`][`tipo`]);
+    console.log(`CUIT: ${cuit} - DESDE: ${periodo_desde} - HASTA: ${periodo_hasta} - TIPO PADRON: ${tipo} \n`);
     if (!cuit) {
         return res.status(400).json({ error: `Falta parametro "cuit"` });
     }
 
-    if (!dia) {
-        return res.status(400).json({ error: `Falta parametro "dia"` });
+    if (!periodo_desde) {
+        return res.status(400).json({ error: `Falta parametro "desde"` });
     }
 
-    if (!mes) {
-        return res.status(400).json({ error: `Falta parametro "mes"` });
+    if (!periodo_hasta) {
+        return res.status(400).json({ error: `Falta parametro "hasta"` });
     }
 
-    if (!ano) {
-        return res.status(400).json({ error: `Falta parametro "año"` });
+    if (!tipo) {
+        return res.status(400).json({ error: `Falta parametro "tipo"` });
     }
-
+    console.log(`CUIT: ${cuit} - DESDE: ${periodo_desde} - HASTA: ${periodo_hasta} - TIPO PADRON: ${tipo} \n`);
     try
     {
-        const result = await sql`select alicuota.razon_social, alicuota.cuit, alicuota.alicuota_percepcion, alicuota.alicuota_retencion from alicuota where alicuota.cuit = ${cuit} and alicuota.periodo_inicio_dia <= ${dia} and alicuota.periodo_inicio_mes = ${mes} and alicuota.periodo_inicio_ano = ${ano} and alicuota.periodo_fin_dia >= ${dia}`;
+        const query = `select alicuota.id as id_bbdd, alicuota.id_tipo as id_tipo_padron, alicuota.cuit, alicuota.razon_social, alicuota.alicuota_percepcion, alicuota.alicuota_retencion, alicuota.periodo_desde, alicuota.periodo_hasta from alicuota where alicuota.cuit = ${cuit.toString()} and alicuota.periodo_desde <= TO_DATE(${periodo_desde.toString()},'yyyy-mm-dd') and alicuota.periodo_hasta >= TO_DATE(${periodo_hasta},'yyyy-mm-dd') and alicuota.id_tipo = ${tipo}`
+        console.log(`query: ${query} \n`);
+        const result = await sql`select alicuota.id as id_bbdd, alicuota.id_tipo as id_tipo_padron, alicuota.cuit, alicuota.razon_social, alicuota.alicuota_percepcion, alicuota.alicuota_retencion, alicuota.periodo_desde, alicuota.periodo_hasta from alicuota where alicuota.cuit = ${cuit.toString()} and alicuota.periodo_desde <= TO_DATE(${periodo_desde.toString()},'yyyy-mm-dd') and alicuota.periodo_hasta >= TO_DATE(${periodo_hasta},'yyyy-mm-dd') and alicuota.id_tipo = ${tipo}`
         res.json(result);
     }
     catch (error)
