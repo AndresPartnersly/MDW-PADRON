fbe65c13157aee64f9a275f4635807b65188774f
Andres Brito <andres.brito@partnersly.com>
2025-04-03 13:18:51 -0400

    Create CARGA PADRON ENTRE RIOS.sql

---
+++ Scripts/CARGA PADRON ENTRE RIOS.sql
@@ -0,0 +1,49 @@
+--EXPRESION REGULAR LIMPIEZA ARCHIVOS
+[^a-zA-Z0-9,;\s]
+
+--CREACION TABLA TEMPORAL
+CREATE TEMP TABLE temp_csv_upload (
+    col1 text,
+    col2 text,
+    col3 text,
+    col4 text,
+	col5 text,
+	col6 text,
+	col7 text,
+	col8 text,
+	col9 text,
+	col10 text,
+	col11 text,
+	col12 text
+	);
+
+--CARGA DE DATA EN TABLA TEMPORAL
+copy temp_csv_upload (col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11, col12) FROM 'C:/Users/Public/Documents/2025_04_ENTRERIOS_PadronRetPer202504.txt' DELIMITER ';' ENCODING 'UTF8';
+
+
+--INSECION DE TABLA TEMPORAL
+INSERT INTO alicuota (id_tipo, cuit, razon_social, alicuota_percepcion, alicuota_retencion, periodo_desde, periodo_hasta)
+SELECT 
+	4 as id_tipo, 
+	TRIM(col4) as cuit,
+	TRIM(col12) as razon_social,
+    REPLACE(TRIM(col8), ',', '.')::numeric percepcion,
+    REPLACE(TRIM(col9), ',', '.')::numeric retencion,
+	to_date((SUBSTRING(TRIM(col2), 5, 4)||'-'||SUBSTRING(TRIM(col2), 3, 2)||'-'||SUBSTRING(TRIM(col2), 1, 2)),'yyyy-mm-dd') periodo_desde,
+	to_date((SUBSTRING(TRIM(col3), 5, 4)||'-'||SUBSTRING(TRIM(col3), 3, 2)||'-'||SUBSTRING(TRIM(col3), 1, 2)),'yyyy-mm-dd') periodo_hasta
+FROM temp_csv_upload;
+
+select alicuota.id,
+	alicuota.id_tipo,
+	alicuota.cuit, 
+	alicuota.razon_social,
+	alicuota.alicuota_percepcion,
+	alicuota.alicuota_retencion,
+	alicuota.periodo_desde,
+	alicuota.periodo_hasta 
+from
+	alicuota 
+where alicuota.cuit = '20000163989' 
+and alicuota.periodo_desde <= TO_DATE('2024-06-01','yyyy-mm-dd')
+and alicuota.periodo_hasta >= TO_DATE('2024-06-01','yyyy-mm-dd')
+and alicuota.id_tipo = 1
