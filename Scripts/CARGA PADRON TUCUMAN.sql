fabc5c4dfffe330145fd2cc667b29d71c184994f
Andres Brito <andres.brito@partnersly.com>
2025-04-07 10:39:23 -0400

    Create CARGA PADRON TUCUMAN.sql

---
+++ Scripts/CARGA PADRON TUCUMAN.sql
@@ -0,0 +1,49 @@
+--EXPRESION REGULAR LIMPIEZA ARCHIVOS
+1. Remover filas de cabecera de la 1 a la 7
+2. Incluir separador de columnas con expresion regular en Node++
+		Buscar: (^.{192}) ==> 192: Posicion columna
+		Reemplazar: \1; => Agrega el punto y coma.
+3. A las lineas de contribuyentes exentos agregar alicuota 0.0 aplicando replace en Node++
+4. Guardar archivo en codificacion UTFC y aplicar expresion regular para remover caracteres invalidos: [^a-zA-Z0-9,;.\s]
+
+--CREACION TABLA TEMPORAL
+CREATE TEMP TABLE temp_csv_upload (
+    col1 text,
+    col2 text,
+    col3 text,
+    col4 text,
+	col5 text,
+	col6 text,
+	col7 text
+	);
+
+--CARGA DE DATA EN TABLA TEMPORAL
+copy temp_csv_upload (col1, col2, col3, col4, col5, col6, col7) FROM 'C:/Users/Public/Documents/2025_03_TUCUMAN.txt' DELIMITER ';' ENCODING 'UTF8';
+
+
+--INSECION DE TABLA TEMPORAL
+INSERT INTO alicuota (id_tipo, cuit, razon_social, alicuota_percepcion, alicuota_retencion, periodo_desde, periodo_hasta)
+SELECT 
+	6 as id_tipo, 
+	TRIM(col1) as cuit,
+	SUBSTRING(TRIM(col6),1,100) as razon_social,
+    REPLACE(TRIM(col7), ',', '.')::numeric percepcion,
+    REPLACE(TRIM(col7), ',', '.')::numeric retencion,
+	to_date((SUBSTRING(TRIM(col4), 1, 4)||'-'||SUBSTRING(TRIM(col4), 5, 2)||'-'||SUBSTRING(TRIM(col4), 7, 2)),'yyyy-mm-dd') periodo_desde,
+	to_date((SUBSTRING(TRIM(col5), 1, 4)||'-'||SUBSTRING(TRIM(col5), 5, 2)||'-'||SUBSTRING(TRIM(col5), 7, 2)),'yyyy-mm-dd') periodo_hasta
+FROM temp_csv_upload;
+
+select alicuota.id,
+	alicuota.id_tipo,
+	alicuota.cuit, 
+	alicuota.razon_social,
+	alicuota.alicuota_percepcion,
+	alicuota.alicuota_retencion,
+	alicuota.periodo_desde,
+	alicuota.periodo_hasta 
+from
+	alicuota 
+where alicuota.cuit = '20000163989' 
+and alicuota.periodo_desde <= TO_DATE('2024-06-01','yyyy-mm-dd')
+and alicuota.periodo_hasta >= TO_DATE('2024-06-01','yyyy-mm-dd')
+and alicuota.id_tipo = 1
