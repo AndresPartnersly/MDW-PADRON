2e6b1a524376f0291f8e4f1f01a360eb20e47107
Andres Brito <andres.brito@partnersly.com>
2025-04-07 23:12:26 -0400

    Update CARGA PADRON TUCUMAN.sql

--- Scripts/CARGA PADRON TUCUMAN.sql
+++ Scripts/CARGA PADRON TUCUMAN.sql
@@ -7,7 +7,7 @@
 4. Guardar archivo en codificacion UTFC y aplicar expresion regular para remover caracteres invalidos: [^a-zA-Z0-9,;.\s]
 
 --CREACION TABLA TEMPORAL
-CREATE TEMP TABLE temp_csv_upload (
+CREATE TEMP TABLE tabla1 (
     col1 text,
     col2 text,
     col3 text,
@@ -18,7 +18,7 @@ CREATE TEMP TABLE temp_csv_upload (
 	);
 
 --CARGA DE DATA EN TABLA TEMPORAL
-copy temp_csv_upload (col1, col2, col3, col4, col5, col6, col7) FROM 'C:/Users/Public/Documents/2025_03_TUCUMAN.txt' DELIMITER ';' ENCODING 'UTF8';
+copy tabla1 (col1, col2, col3, col4, col5, col6, col7) FROM 'C:/Users/Public/Documents/2025_04_TUCUMAN_ACREDITAN.txt' DELIMITER ';' ENCODING 'UTF8';
 
 
 --INSECION DE TABLA TEMPORAL
@@ -31,19 +31,30 @@ SELECT
     REPLACE(TRIM(col7), ',', '.')::numeric retencion,
 	to_date((SUBSTRING(TRIM(col4), 1, 4)||'-'||SUBSTRING(TRIM(col4), 5, 2)||'-'||SUBSTRING(TRIM(col4), 7, 2)),'yyyy-mm-dd') periodo_desde,
 	to_date((SUBSTRING(TRIM(col5), 1, 4)||'-'||SUBSTRING(TRIM(col5), 5, 2)||'-'||SUBSTRING(TRIM(col5), 7, 2)),'yyyy-mm-dd') periodo_hasta
-FROM temp_csv_upload;
+FROM tabla1;
 
-select alicuota.id,
-	alicuota.id_tipo,
-	alicuota.cuit, 
-	alicuota.razon_social,
-	alicuota.alicuota_percepcion,
-	alicuota.alicuota_retencion,
-	alicuota.periodo_desde,
-	alicuota.periodo_hasta 
-from
-	alicuota 
-where alicuota.cuit = '20000163989' 
-and alicuota.periodo_desde <= TO_DATE('2024-06-01','yyyy-mm-dd')
-and alicuota.periodo_hasta >= TO_DATE('2024-06-01','yyyy-mm-dd')
-and alicuota.id_tipo = 1
+
+--TABLA TEMPORAL COEFICIENTES
+CREATE TEMP TABLE tabla2 (
+    col1 text,
+    col2 text,
+    col3 text,
+    col4 text,
+	col5 text,
+	col6 text
+	);
+
+	copy tabla2 (col1, col2, col3, col4, col5, col6) FROM 'C:/Users/Public/Documents/2025_04_TUCUMAN_archivocoefrg116.txt' DELIMITER ';' ENCODING 'UTF8';
+
+
+--TABLA TEMPORAL ANEXOS
+CREATE TEMP TABLE tabla3 (
+    col1 text,
+    col2 text,
+	col3 text
+	);
+
+	copy tabla3 (col1, col2, col3) FROM 'C:/Users/Public/Documents/2025_04_TUCUMAN_padron_anexo_x_rg_23-02.txt' DELIMITER ';' ENCODING 'UTF8';
+
+select tabla1.col1 as cuit, tabla1.col4 as desde, tabla1.col5 as hasta, tabla1.col6 as razon_social, tabla1.col7 as alicuota, tabla2.col6 as coeficiente, tabla3.col1 as anexo from tabla1 left join tabla2 on tabla1.col1 = tabla2.col1 left join tabla3 on tabla1.col1 = tabla3.col1 where tabla3.col1 is not null
+	
