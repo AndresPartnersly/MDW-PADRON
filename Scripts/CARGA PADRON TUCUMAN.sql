7d36c6855a94eb7c7dfbb5eaf62727c215fd52c9
Andres Brito <andres.brito@partnersly.com>
2025-05-06 17:46:14 -0400

    Update CARGA PADRON TUCUMAN.sql

--- Scripts/CARGA PADRON TUCUMAN.sql
+++ Scripts/CARGA PADRON TUCUMAN.sql
@@ -2,9 +2,10 @@
 1. Remover filas de cabecera de la 1 a la 7
 2. Incluir separador de columnas con expresion regular en Node++
 		Buscar: (^.{192}) ==> 192: Posicion columna--11,17,22,31,42,192
-		Reemplazar: \1; => Agrega el punto y coma.--Reemplazar "-----" por "0    "
-3. A las lineas de contribuyentes exentos agregar alicuota 0.0 aplicando replace en Node++
+		Reemplazar: \1; => Agrega el punto y coma.
+3. A las lineas de contribuyentes exentos agregar alicuota 0.0 aplicando replace en Node++--Reemplazar "-----" por "0    "
 4. Guardar archivo en codificacion UTFC y aplicar expresion regular para remover caracteres invalidos: [^a-zA-Z0-9,;.\s]
+
 
 --CREACION TABLA TEMPORAL
 CREATE TEMP TABLE tabla1 (
@@ -22,17 +23,25 @@ copy tabla1 (col1, col2, col3, col4, col5, col6, col7) FROM 'C:/Users/Public/Doc
 
 
 --INSECION DE TABLA TEMPORAL
-INSERT INTO alicuota (id_tipo, cuit, razon_social, alicuota_percepcion, alicuota_retencion, periodo_desde, periodo_hasta)
+INSERT INTO alicuota (id_tipo, cuit, razon_social, alicuota_percepcion, alicuota_retencion, periodo_desde, periodo_hasta,coeficiente)
 SELECT 
 	6 as id_tipo, 
-	TRIM(col1) as cuit,
-	SUBSTRING(TRIM(col6),1,100) as razon_social,
-    REPLACE(TRIM(col7), ',', '.')::numeric percepcion,
-    REPLACE(TRIM(col7), ',', '.')::numeric retencion,
-	to_date((SUBSTRING(TRIM(col4), 1, 4)||'-'||SUBSTRING(TRIM(col4), 5, 2)||'-'||SUBSTRING(TRIM(col4), 7, 2)),'yyyy-mm-dd') periodo_desde,
-	to_date((SUBSTRING(TRIM(col5), 1, 4)||'-'||SUBSTRING(TRIM(col5), 5, 2)||'-'||SUBSTRING(TRIM(col5), 7, 2)),'yyyy-mm-dd') periodo_hasta
-FROM tabla1;
+	TRIM(COALESCE(tabla1.col1,tabla2.col1)) as cuit,
+	TRIM(COALESCE(tabla1.col6,tabla2.col7)) as razon_social,
+    REPLACE(TRIM(tabla1.col7), ',', '.')::numeric percepcion,
+    REPLACE(TRIM(tabla1.col7), ',', '.')::numeric retencion,
+	to_date((SUBSTRING(TRIM('20250501'), 1, 4)||'-'||SUBSTRING(TRIM('20250501'), 5, 2)||'-'||SUBSTRING(TRIM('20250501'), 7, 2)),'yyyy-mm-dd') periodo_desde,
+	to_date((SUBSTRING(TRIM('20250531'), 1, 4)||'-'||SUBSTRING(TRIM('20250531'), 5, 2)||'-'||SUBSTRING(TRIM('20250531'), 7, 2)),'yyyy-mm-dd') periodo_hasta,
+	REPLACE(TRIM(tabla2.col3), ',', '.')::numeric coeficiente
+FROM tabla1 full join tabla2 on tabla1.col1 = tabla2.col1;
 
+--COEFICIENTES
+1. Remover filas de cabecera de la 1 a la 7
+2. Incluir separador de columnas con expresion regular en Node++
+		Buscar: (^.{192}) ==> 192: Posicion columna--11,17,24, 27, 34, 37, 190
+		Reemplazar: \1; => Agrega el punto y coma.
+3. A las lineas de contribuyentes exentos agregar alicuota 0.0 aplicando replace en Node++--Reemplazar "-.----" por "0.0000"
+4. Guardar archivo en codificacion UTFC y aplicar expresion regular para remover caracteres invalidos: [^a-zA-Z0-9,;.\s]
 
 --TABLA TEMPORAL COEFICIENTES
 CREATE TEMP TABLE tabla2 (
@@ -41,10 +50,12 @@ CREATE TEMP TABLE tabla2 (
     col3 text,
     col4 text,
 	col5 text,
-	col6 text
+	col6 text,
+	col7 text,
+	col8 text
 	);
 
-	copy tabla2 (col1, col2, col3, col4, col5, col6) FROM 'C:/Users/Public/Documents/2025_04_TUCUMAN_archivocoefrg116.txt' DELIMITER ';' ENCODING 'UTF8';
+	copy tabla2 (col1, col2, col3, col4, col5, col6, col7, col8) FROM 'C:/Users/Public/Documents/2025_04_TUCUMAN_archivocoefrg116.txt' DELIMITER ';' ENCODING 'UTF8';
 
 
 --TABLA TEMPORAL ANEXOS
@@ -58,6 +69,10 @@ CREATE TEMP TABLE tabla3 (
 
 --SELECT A TABLA TEMPORAL
 select tabla1.col1 as cuit, tabla1.col4 as desde, tabla1.col5 as hasta, tabla1.col6 as razon_social, tabla1.col7 as alicuota, tabla2.col6 as coeficiente, tabla3.col1 as anexo from tabla1 left join tabla2 on tabla1.col1 = tabla2.col1 left join tabla3 on tabla1.col1 = tabla3.col1 where tabla3.col1 is not null
+
+--Usada el 06/05/2025
+--select tabla1.col1 as cuit, tabla1.col4 as desde, tabla1.col5 as hasta, tabla1.col6 as razon_social, tabla1.col7 as alicuota, tabla2.col6 as coeficiente from tabla1 left join tabla2 on tabla1.col1 = tabla2.col1
+
 
 --SELECT A TABLA ALICUOTA CRUZADA CON TABLATIPO
 SELECT alicuota.id, alicuota.id_tipo, tipo.nombre as padron, alicuota.cuit, alicuota.razon_social, alicuota.alicuota_percepcion, alicuota.alicuota_retencion, alicuota.periodo_desde, alicuota.periodo_hasta
