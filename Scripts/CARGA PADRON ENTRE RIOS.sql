8094dd8a021304d66aae0d53f556f8b30ede7fa8
Andres Brito <andres.brito@partnersly.com>
2025-05-30 06:37:53 -0400

    Update CARGA PADRON ENTRE RIOS.sql

--- Scripts/CARGA PADRON ENTRE RIOS.sql
+++ Scripts/CARGA PADRON ENTRE RIOS.sql
@@ -1,8 +1,10 @@
 --EXPRESION REGULAR LIMPIEZA ARCHIVOS
 [^a-zA-Z0-9,;\s]
 
+--remover primera fila de cabecera
+
 --CREACION TABLA TEMPORAL
-CREATE TEMP TABLE temp_csv_upload (
+CREATE TEMP TABLE temp_entre_rios (
     col1 text,
     col2 text,
     col3 text,
@@ -18,7 +20,9 @@ CREATE TEMP TABLE temp_csv_upload (
 	);
 
 --CARGA DE DATA EN TABLA TEMPORAL
-copy temp_csv_upload (col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11, col12) FROM 'C:/Users/Public/Documents/2025_04_ENTRERIOS_PadronRetPer202504.txt' DELIMITER ';' ENCODING 'UTF8';
+copy temp_entre_rios (col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11, col12) FROM 'C:/Users/Public/Documents/02-ENTRE RIOS/06-2025/PadronRetPer202506.txt' DELIMITER ';' ENCODING 'UTF8';
+
+
 
 
 --INSECION DE TABLA TEMPORAL
@@ -31,7 +35,7 @@ SELECT
     REPLACE(TRIM(col9), ',', '.')::numeric retencion,
 	to_date((SUBSTRING(TRIM(col2), 5, 4)||'-'||SUBSTRING(TRIM(col2), 3, 2)||'-'||SUBSTRING(TRIM(col2), 1, 2)),'yyyy-mm-dd') periodo_desde,
 	to_date((SUBSTRING(TRIM(col3), 5, 4)||'-'||SUBSTRING(TRIM(col3), 3, 2)||'-'||SUBSTRING(TRIM(col3), 1, 2)),'yyyy-mm-dd') periodo_hasta
-FROM temp_csv_upload;
+FROM temp_entre_rios;
 
 select alicuota.id,
 	alicuota.id_tipo,
