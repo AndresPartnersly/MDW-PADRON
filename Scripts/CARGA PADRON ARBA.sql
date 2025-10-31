0ce17be282a50c5ec82b1e76fee3b15174c20a73
Andres Brito <andres.brito@partnersly.com>
2025-10-31 10:26:14 -0400

    Update CARGA PADRON ARBA.sql

--- Scripts/CARGA PADRON ARBA.sql
+++ Scripts/CARGA PADRON ARBA.sql
@@ -16,7 +16,7 @@ CREATE TEMP TABLE temp_percepciones (
 	col10 text
 	);
 
-copy temp_percepciones FROM 'C:/Users/Public/Documents/04-ARBA/10-2025/PadronRGS102025/PadronRGSPer102025.TXT' WITH (
+copy temp_percepciones FROM 'C:/Users/Public/Documents/04-ARBA/11-2025/PadronRGS112025/PadronRGSPer112025.TXT' WITH (
   FORMAT csv,
   DELIMITER ';',
   HEADER FALSE
@@ -36,7 +36,7 @@ CREATE TEMP TABLE temp_retenciones (
 	col10 text
 	);
 
-copy temp_retenciones FROM 'C:/Users/Public/Documents/04-ARBA/10-2025/PadronRGS102025/PadronRGSRet102025.TXT' WITH (
+copy temp_retenciones FROM 'C:/Users/Public/Documents/04-ARBA/11-2025/PadronRGS112025/PadronRGSRet112025.TXT' WITH (
   FORMAT csv,
   DELIMITER ';',
   HEADER FALSE
@@ -51,6 +51,6 @@ SELECT
 	TRIM(COALESCE(temp_percepciones.col5,temp_retenciones.col5)) as razon_social,
     REPLACE(TRIM(COALESCE(temp_percepciones.col9,null)), ',', '.')::numeric percepcion,
 	REPLACE(TRIM(COALESCE(temp_retenciones.col9,null)), ',', '.')::numeric retencion,
-	to_date((SUBSTRING(TRIM('20251001'), 1, 4)||'-'||SUBSTRING(TRIM('20251001'), 5, 2)||'-'||SUBSTRING(TRIM('20251001'), 7, 2)),'yyyy-mm-dd') periodo_desde,
-	to_date((SUBSTRING(TRIM('20251031'), 1, 4)||'-'||SUBSTRING(TRIM('20251031'), 5, 2)||'-'||SUBSTRING(TRIM('20251031'), 7, 2)),'yyyy-mm-dd') periodo_hasta
+	to_date((SUBSTRING(TRIM('20251101'), 1, 4)||'-'||SUBSTRING(TRIM('20251101'), 5, 2)||'-'||SUBSTRING(TRIM('20251101'), 7, 2)),'yyyy-mm-dd') periodo_desde,
+	to_date((SUBSTRING(TRIM('20251130'), 1, 4)||'-'||SUBSTRING(TRIM('20251130'), 5, 2)||'-'||SUBSTRING(TRIM('20251130'), 7, 2)),'yyyy-mm-dd') periodo_hasta
 FROM temp_percepciones full join temp_retenciones on temp_percepciones.col5 = temp_retenciones.col5
