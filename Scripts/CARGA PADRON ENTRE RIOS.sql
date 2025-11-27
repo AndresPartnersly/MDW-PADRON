405a78eb834cd917995fae9434a020b65811efa3
Andres Brito <andres.brito@partnersly.com>
2025-11-27 11:52:39 -0400

    Update CARGA PADRON ENTRE RIOS.sql

--- Scripts/CARGA PADRON ENTRE RIOS.sql
+++ Scripts/CARGA PADRON ENTRE RIOS.sql
@@ -20,7 +20,7 @@ CREATE TEMP TABLE temp_entre_rios (
 	);
 
 --CARGA DE DATA EN TABLA TEMPORAL
-copy temp_entre_rios (col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11, col12) FROM 'C:/Users/Public/Documents/02-ENTRE RIOS/11-2025/PadronRetPer202511.txt' DELIMITER ';' ENCODING 'UTF8';
+copy temp_entre_rios (col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11, col12) FROM 'C:/Users/Public/Documents/02-ENTRE RIOS/12-2025/PadronRetPer202512.txt' DELIMITER ';' ENCODING 'UTF8';
 
 
 
