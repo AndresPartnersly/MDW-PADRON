22df6a8b7368d69439641ecd17c806f181c560a4
Andres Brito <andres.brito@partnersly.com>
2026-02-26 17:48:09 -0400

    Update CARGA PADRON AGIP.sql

--- Scripts/CARGA PADRON AGIP.sql
+++ Scripts/CARGA PADRON AGIP.sql
@@ -19,7 +19,7 @@ CREATE TEMP TABLE temp_csv_upload (
 	);
 
 --CARGA DE DATA EN TABLA TEMPORAL
-copy temp_csv_upload (col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11, col12) FROM 'C:/Users/Public/Documents/01-AGIP/02-2026/ARDJU008022026/ARDJU008022026.txt' DELIMITER ';' ENCODING 'UTF8';
+copy temp_csv_upload (col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11, col12) FROM 'C:/Users/Public/Documents/01-AGIP/03-2026/ARDJU008032026/ARDJU008032026.txt' DELIMITER ';' ENCODING 'UTF8';
 
 
 --INSECION DE TABLA TEMPORAL
