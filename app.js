9fbcbbfa2ed67c29653f1a9c1565ca06c2355432
Facundo Galleguillo <facundo.galleguillo@partnersly.com>
2026-07-13 13:18:53 -0300

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const axios = require('axios');
 // Cargar variables de entorno desde el archivo .env
 dotenv.config();
 
+const { chromium } = require('playwright');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -30,7 +31,7 @@ app.get('/search', async (req, res) => {
     const periodo_desde = req[`query`][`desde`];
     const periodo_hasta = req[`query`][`hasta`];
     const tipo = parseInt(req[`query`][`tipo`]);
-    console.log(`33. CUIT: ${cuit} - DESDE: ${periodo_desde} - HASTA: ${periodo_hasta} - TIPO PADRON: ${tipo} \n`);
+    console.log(`34. CUIT: ${cuit} - DESDE: ${periodo_desde} - HASTA: ${periodo_hasta} - TIPO PADRON: ${tipo} \n`);
     if (!cuit) {
         return res.status(400).json({ error: `Falta parametro "cuit"` });
     }
@@ -46,10 +47,10 @@ app.get('/search', async (req, res) => {
     if (!tipo) {
         return res.status(400).json({ error: `Falta parametro "tipo"` });
     }
-    console.log(`49. CUIT: ${cuit} - DESDE: ${periodo_desde} - HASTA: ${periodo_hasta} - TIPO PADRON: ${tipo} \n`);
+    console.log(`50. CUIT: ${cuit} - DESDE: ${periodo_desde} - HASTA: ${periodo_hasta} - TIPO PADRON: ${tipo} \n`);
     try {
         const query = `select alicuota.id as id_bbdd, alicuota.id_tipo as id_tipo_padron, alicuota.cuit, alicuota.razon_social, alicuota.alicuota_percepcion, alicuota.alicuota_retencion, alicuota.periodo_desde, alicuota.periodo_hasta from alicuota where alicuota.cuit = ${cuit.toString()} and alicuota.periodo_desde <= TO_DATE(${periodo_desde.toString()},'yyyy-mm-dd') and alicuota.periodo_hasta >= TO_DATE(${periodo_hasta},'yyyy-mm-dd') and alicuota.id_tipo = ${tipo}`
-        console.log(`52. query: ${query} \n`);
+        console.log(`53. query: ${query} \n`);
         const result = await sql`select alicuota.id as id_bbdd, alicuota.id_tipo as id_tipo_padron, alicuota.cuit, alicuota.razon_social, alicuota.alicuota_percepcion, alicuota.alicuota_retencion, alicuota.periodo_desde, alicuota.periodo_hasta from alicuota where alicuota.cuit = ${cuit.toString()} and alicuota.periodo_desde <= TO_DATE(${periodo_desde.toString()},'yyyy-mm-dd') and alicuota.periodo_hasta >= TO_DATE(${periodo_hasta},'yyyy-mm-dd') and alicuota.id_tipo = ${tipo}`
         res.json(result);
     }
@@ -62,60 +63,120 @@ app.get('/search', async (req, res) => {
     }
 });
 
-// Endpoint para obtener el documento html de una pagina web creado principalmente para servicio de actualizacion de tipo de cambio (Localizaciones ARG)
-app.get('/get_html_data_from_url', async (req, res) => {
+// // Endpoint para obtener el documento html de una pagina web creado principalmente para servicio de actualizacion de tipo de cambio (Localizaciones ARG)
+// app.get('/get_html_data_from_url', async (req, res) => {
+
+//     let response = { error: true, message: null, response: null };
+//     let message = ``;
+
+//     try {
+
+//         const requestParameters = req.query;
+//         console.log(`75. Request Params: ${JSON.stringify(requestParameters)}`);
+//         const baseUrl = requestParameters.base_url;
+//         console.log(`77. Request Base URL To Get: ${baseUrl}`);
+
+//         if (!isEmpty(baseUrl)) {
+
+//             let htmlData = await axios.get(baseUrl);
+
+//             if (!isEmpty(htmlData)) {
+
+//                 console.log(`85. Request Response Code: ${htmlData.status}`);
+
+//                 let docHtml = htmlData.data;
+//                 console.log(`88. HTML Body: ${docHtml}`);
+
+//                 if (!isEmpty(docHtml)) {
+//                     response.response = docHtml;
+//                     response.error = false;
+//                     response.message = `Solicitud realizada con exito.`;
+//                     res.status(200).json(response);
+//                 }
+//                 else {
+//                     response.error = true;
+//                     response.message = `Error: no se pudo obtener html de la web indicada.`;
+//                     res.status(500).json(response);
+//                 }
+//             }
+//             else {
+//                 response.error = true;
+//                 response.message = `Error: parametro "base_url" está vacío o incorrectamente cargado.`;
+//                 res.status(500).json(response);
+//             }
+//         }
+//     }
+//     catch (e) {
+//         message = `Error: ${e.message}`;
+//         response.message = message;
+//         res.status(500).json(response);
+//     }
+//     return response;
+// });
+
+// Endpoint para obtener el documento html de una pagina web creado principalmente para servicio de actualizacion de tipo de cambio (Localizaciones ARG) con Playwright para evitar validaciones de cookies/bots
+app.get('/get_html_from_url', async (req, res) => {
 
     let response = { error: true, message: null, response: null };
     let message = ``;
+    let browser;
 
     try {
 
+        let browser = await chromium.launch({
+            headless: true
+        });
+
         const requestParameters = req.query;
-        console.log(`74. Request Params: ${JSON.stringify(requestParameters)}`);
+        console.log(`126. Request Params: ${JSON.stringify(requestParameters)}`);
         const baseUrl = requestParameters.base_url;
-        console.log(`76. Request Base URL To Get: ${baseUrl}`);
+        console.log(`128. Request Base URL To Get: ${baseUrl}`);
 
-        if (!isEmpty(baseUrl)) {
+        const { base_url } = req.query;
 
-            let htmlData = await axios.get(baseUrl);
+        const context = await browser.newContext({
 
-            if (!isEmpty(htmlData)) {
+            userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/150.0.0.0 Safari/537.36',
 
-                console.log(`84. Request Response Code: ${htmlData.status}`);
+            viewport: {
+                width: 1920,
+                height: 1080
+            },
 
-                let docHtml = htmlData.data;
-                console.log(`87. HTML Body: ${docHtml}`);
+            locale: 'es-AR',
+            timezoneId: 'America/Argentina/Buenos_Aires'
 
-                if (!isEmpty(docHtml)) {
-                    response.response = docHtml;
-                    response.error = false;
-                    response.message = `Solicitud realizada con exito.`;
-                    res.status(200).json(response);
-                }
-                else {
-                    response.error = true;
-                    response.message = `Error: no se pudo obtener html de la web indicada.`;
-                    res.status(500).json(response);
-                }
-            }
-            else {
-                response.error = true;
-                response.message = `Error: parametro "base_url" está vacío o incorrectamente cargado.`;
-                res.status(500).json(response);
-            }
+        });
+
+        const page = await context.newPage();
+
+        await page.goto(base_url, {
+            waitUntil: 'networkidle',
+            timeout: 30000
+        });
+
+        const html = await page.content();
+        response.error = false;
+        response.message = `Solicitud realizada con exito.`;
+        response.response = html;
+        return res.status(200).json(response);
+    }
+    catch (err) {
+        console.error(err);
+        response.error = true;
+        response.message = `Error: ${err.message}`;
+        return res.status(500).json(response);
+    }
+    finally {
+        if (browser) {
+            await browser.close();
         }
     }
-    catch (e) {
-        message = `Error: ${e.message}`;
-        response.message = message;
-        res.status(500).json(response);
-    }
-    return response;
 });
 
 // Iniciar el servidor
 app.listen(port, () => {
-    console.log(`118. Servidor corriendo en el puerto ${port}`);
+    console.log(`188. Servidor corriendo en el puerto ${port}`);
 });
 
 let isEmpty = (value) => {
